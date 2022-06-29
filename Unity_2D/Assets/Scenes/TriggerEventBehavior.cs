4ace05991ff9d933d235d703bfd52af1d2a1c827
Brandon Ainge <10888736@uvu.edu>
2022-06-28 21:51:28 -0600

    Added a project and started following video tutorials

---
+++ Unity_2D/Assets/Scenes/TriggerEventBehavior.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+	public UnityEvent triggerEnterEvent;
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		triggerEnterEvent.Invoke();
+	}
+}
